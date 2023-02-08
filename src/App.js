d044050bd66c5c236b1908f426aa32ba6dae8bd6
Iran <iransaboonian@gmail.com>
2023-02-08 22:17:24 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const initArr=["thing 1","thing 2"].map(value=>{return <p>{value}</p>});
+  const [Thing,setThing]=React.useState(initArr);
+  function handleClick(){
+    setThing(
+      (prevThing)=>{
+        return [...prevThing,`Thing ${prevThing.length+1}`].map(value=>{return <p>{value}</p>})}
+        )
+      
+      };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="btn-div">
+        <button onClick={handleClick}>Add Thing</button>
+      </div>
+      <div className="things">
+        {Thing}
+      </div>
     </div>
   );
 }
